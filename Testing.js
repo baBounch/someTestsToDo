2799a4b0c8148975e3e50ed09ec13911a46902fc
Shane Delaney <shanebdelaneyii@gmail.com>
2017-06-17 18:18:27 -0230

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -1,5 +1,5 @@
-//$(document).ready(function(){
-  //var gg = 200;
-  //$("#containing-element").css("background-color","blue");
-//})
-console.log("pizza");
+$(document).ready(function(){
+  var gg = 200;
+  $("#containing-element").css("background-color","blue");
+})
+
