ce2d1a33c4d2200629fcd3db04f1cb14b6114453
Shane Delaney <shanebdelaneyii@gmail.com>
2017-06-17 18:43:07 -0230

    Update general-page.js

--- js/general-page.js
+++ js/general-page.js
@@ -1,7 +1,9 @@
 $(document).ready(()=>{
 
-
-
+  var generalHeader = document.getElementById("general-header");
+  generalHeader.innerHTML = "<div class='row'>"+
+    "<button>Section 1</button>"+
+    "</div>";
 
 
 })
